4c797d00fca4b87c5540647f17d348001019144b
NehuelNF <alfaronelson748@gmail.com>
2024-08-30 20:00:44 -0400

    Comienzo capetas y botones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: 'splash',
+    loadChildren: () => import('./splash/splash.module').then( m => m.SplashPageModule)
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'splash',
     pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
 ];
 
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SessionManager } from 'src/managers/SessionManager';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private router: Router, private sessionManager : SessionManager) { }
+
+    user: string = '';
+    password: string = '';
+
+  ngOnInit() {
+  }
+
+  onLoginButtonPressed(){
+    if (this.sessionManager.performLogin(this.user, this.password)){
+      this.router.navigate(['/home'])
+    } else {
+      alert('El usuario o contraseña ingresada son invalidas.')
+    }
+  }
+
+  onRegisterButtonPressed(){
+    this.router.navigate(['/register'])
+  }
+
+}
